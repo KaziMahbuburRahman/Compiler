1ea24e2329be23f11fc5a99c3a73893cb28047b0
NoobMahbub <Mahbubbengali@gmail.com>
2024-01-26 01:40:21 +0600

    added preloader to all components

--- src/Layouts/Root.jsx
+++ src/Layouts/Root.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { NavbarComponent } from '../components/NavbarComponent'
 import Landing from '../components/Landing'
 import Footer from '../components/Footer'
 
 import ThemeChanger from '../shared/ThemeChanger/ThemeChanger';
+import Preloader from '../components/Preloader';
 
 const Root = () => {
+  const [loading, setLoading] = useState(false);
   const [isModalOpen, setModalOpen] = useState(false);
   const openModal = () => {
     setModalOpen(true);
@@ -14,15 +16,32 @@ const Root = () => {
     setModalOpen(false);
   }
   const [daisyTheme, setDaisyTheme] = useState('');
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
-    <div> <>
-      <NavbarComponent openModal={openModal} />
-      <ThemeChanger isModalOpen={isModalOpen} closeModal={closeModal} daisyTheme={daisyTheme} setDaisyTheme={setDaisyTheme} />
-      <Landing />
-      
-      <Footer />
+    <>
+      {
+        loading ? (
+          <Preloader />
+        ) :
+          (
+            <div> <>
+              <NavbarComponent openModal={openModal} />
+              <ThemeChanger isModalOpen={isModalOpen} closeModal={closeModal} daisyTheme={daisyTheme} setDaisyTheme={setDaisyTheme} />
+              <Landing />
+
+              <Footer />
+            </>
+            </div>
+          )
+      }
     </>
-    </div>
 
   )
 }
